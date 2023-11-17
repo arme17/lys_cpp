982cdf7567748f73eb4b7a8f21ad218a3a00a6d6
arme17 <liuyishan17@163.com>
2023-11-17 22:43:47 +0800

    提交文件

--- test1.cpp
+++ test1.cpp
@@ -5,64 +5,131 @@
 using namespace std;
 int main()
 {
-    //switch语句给电影打分
-    //经典10-9 8-7非常好，6-5一般 5一下烂片
-    cout << "请给电影评分" << endl;
-    int score = 0;
-    cin >> score;
-    cout << "您打的分数为" << score << endl;
-    //根据用户输入的分数来提示用户最后的结果
-
-    int num = 0;
-    //while括号内写循环结束条件
-   /* while (num < 10)
-    {
-        num++;
-        cout << num << endl;
-    }*/
-    //系统随机生成1-100 ，提示猜大了还是小了，猜对了提示胜利
-    // 添加随机数种子，利用时间生成随机数
-    srand((unsigned int)time(NULL));
-
-    //系统生成随机数
-    num = rand() % 100+1;
-   // cout << num << endl;
-    //玩家猜测
-    int val = 0;
-    cout << "请输入猜测数字" << endl;
-    int i = 0;
-    while (i<5)
-    {
-        cin >> val;
-        i++;
-        if (val > num)
-        {
-            cout << "猜测过大" << endl;
-
-        }
-        else if (val < num)
-        {
-            cout << "猜测过小" << endl;
-        }
-        else
-        {
-            cout << "恭喜猜对了" << endl;
-            break;
-        }
-
-    }
     
-   
-    
+   //嵌套循环实现星图
+	for (int  i = 0; i < 10; i++)
+	{
+		
+		for (int j = 0; j < 10; j++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+
+	}
+	//打印99乘法表
+	for (int i = 1; i < 10; i++)
+	{
+		for (int  j = 1; j < 10; j++)
+		{
+			if (i >= j)
+			{
+				cout << i << "*" << j << "=" << i * j;
+			}
+			
+		}
+		cout << endl;
+	}
+
+	//break使用时机
+	//1.在switch语句中用break
+	cout << "请选择副本难度" << endl;
+	int select = 0;
+	cin >> select;
+	
+	
+	switch (select)
+	{
+	case 1:
+		cout << "mormal level" << endl;
+		break;
+	case 2:
+		cout << "medium level" << endl;
+		break;
+	case 3:
+		cout << "hard level" << endl;
+		break;
+
+	default:
+		break;
+	}
+
+	//2出现再循环语句中  //3嵌套循环语句
+	for (int i = 0; i < 10; i++)
+	{
+		goto Flag;
+	    Flag:
+		if (i == 5)
+		{
+			break;//跳出循环语句
+		}
+		cout << i << endl;
+
+	}
+
+	//continue语句：在循环语句中跳过本次循环，执行余下未执行的语句
+	//break跳出循环，不再循环了
+	/*for (int i = 0; i < 100; i++)
+	{
+		if (i%2 ==0)
+		{
+			continue;
+
+		}
+		cout << i << endl;
+	}*/
+	//arr[5]五个数，但是下标从0开始
+	/*int arr[5];
+	arr[0] = 10;
+	arr[1] = 11;
+	arr[2] = 12;
+	arr[3] = 13;
+	arr[4] = 14;*/
+
+	//
+	int arr2[5] = { 10,20,30,40,50 };
+	cout << arr2[0] << endl;
+	for (int i = 0; i < 5; i++)
+	{
+		cout << arr2[i] << endl;
+	}
+	int arr3[] = { 20,30,30,40 };
+
+
+	//数组名称的用途
+	//1 通过数组明统计整个数组占用内存大小
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	cout << "整个数组占用内存空间为：" << sizeof(arr) << endl;
+	cout << "每个元素所占的内存空间为：" << sizeof(arr[0]) << endl;
+	cout << "元素个数" << sizeof(arr) / sizeof(arr[0]) << endl;
+
+
+	//
+
+
+	//2通过数组名查看数组的首地址
+	cout << "数组首地址" << arr << endl;
+	cout << "数组首地址" << (int)arr << endl;
+	cout << "数组第一个元素首地址" << (int)&arr[0] << endl;
+	cout << "数组第二个元素首地址" << (int)&arr[1] << endl;
+
+	//五个小猪称体重取最重
+	int arr4[5] = { 200,300,280,400,300 };
+	int max = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		//认定一个最大值
+		if (arr4[i] > max)
+		{
+			max = arr4[i];
+		}
+
+	}
+	cout << "最重小猪为" << max << endl;
+
+	//数组元素逆
+
+	
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
