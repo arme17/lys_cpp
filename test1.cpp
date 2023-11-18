1c6936d8fa46f9adcf2371fb81cd94f19ff5b878
arme17 <liuyishan17@163.com>
2023-11-18 14:10:08 +0800

    提交文件

--- test1.cpp
+++ test1.cpp
@@ -1,135 +1,106 @@
 ﻿// test1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //time时间系统头文件的包含
-#include<ctime>
+
 #include <iostream>
+#include<string.h>
 using namespace std;
 int main()
 {
-    
-   //嵌套循环实现星图
-	for (int  i = 0; i < 10; i++)
-	{
-		
-		for (int j = 0; j < 10; j++)
-		{
-			cout << "*";
-		}
-		cout << endl;
+	////////实现数组逆置
+	////////1创建数组
+	//////int arr1[5] = { 1,2,3,4,5 };
+	//////cout << "原数组为" << endl;
 
-	}
-	//打印99乘法表
-	for (int i = 1; i < 10; i++)
+	//////for (int i = 0; i < 5; i++)
+	//////{
+	//////	cout << arr1[i] << endl;
+
+	//////}
+
+	////////2实现逆置
+ //////  //2.1定义数组的下标start 和end
+	////////2.2利用temp存储交换前的数组
+	////////2.3利用循环遍历
+
+	//////int start = 0;
+	//////int end = sizeof(arr1) / sizeof(arr1[0])-1;
+	//////while (start<end)
+	//////{
+	//////	int temp = arr1[start];
+	//////	arr1[start] = arr1[end];
+	//////	arr1[end] = temp;
+	//////	
+	//////	start++;
+	//////	end--;
+	//////}
+
+
+
+	////////3打印逆置后的数组
+ //////   
+	//////cout << "逆数组为" << endl;
+
+	//////for (int i = 0; i < 5; i++)
+	//////{
+	//////	cout << arr1[i] << endl;
+
+	//////}
+
+	
+	////冒泡排序
+	//int arr1[9] = { 2,4,0,5,7,9,8,3,1 };
+	//cout << "排序前的数组为" << endl;
+	//for (int i = 0; i < 9; i++)
+	//{
+	//	cout << arr1[i] << endl;
+	//}
+	//int temp = 0;
+	//for (int i = 0; i < 9-1; i++)
+	//{
+	//	for (int j = 0; j < 9-i-1; j++)
+	//	{
+	//		if (arr1[j] > arr1[j + 1])
+	//		{
+	//			temp = arr1[j];
+	//			arr1[j] = arr1[j + 1];
+	//			arr1[j + 1] = temp;
+	//		}
+
+	//	}
+	//}
+	//cout << "排序后的数组为" << endl;
+	//for (int i = 0; i < 9; i++)
+	//{
+	//	cout << arr1[i];
+	//}
+	//int arr[][3] = { 1,2,3,4,5,6 };
+	//cout << sizeof(arr[0][0]) << endl;
+
+	////二维数组名，查看数组所占的内存空间，获取二维数组的首地址
+	//cout << "第一个元素首地址" << (int)&arr[0][0] << endl;
+	//
+	//二维数组案例
+
+	//考试成绩统计
+
+	int scores[3][3] = {
+		{100,100,100},
+		{90,50,100},
+		{60,70,80}
+	};
+	string names[3] = { "zhangsan", "lisi","wangwu"};
+	//统计每个人的总和分数
+	for (int  i = 0; i < 3; i++)
 	{
-		for (int  j = 1; j < 10; j++)
+		int sum = 0;//统计分数总和变量
+		for (int j = 0; j < 3; j++)
 		{
-			if (i >= j)
-			{
-				cout << i << "*" << j << "=" << i * j;
-			}
+			sum += scores[i][j];
 			
 		}
+		cout <<names[i]<< "总分为" << sum << " ";
 		cout << endl;
 	}
-
-	//break使用时机
-	//1.在switch语句中用break
-	cout << "请选择副本难度" << endl;
-	int select = 0;
-	cin >> select;
-	
-	
-	switch (select)
-	{
-	case 1:
-		cout << "mormal level" << endl;
-		break;
-	case 2:
-		cout << "medium level" << endl;
-		break;
-	case 3:
-		cout << "hard level" << endl;
-		break;
-
-	default:
-		break;
-	}
-
-	//2出现再循环语句中  //3嵌套循环语句
-	for (int i = 0; i < 10; i++)
-	{
-		goto Flag;
-	    Flag:
-		if (i == 5)
-		{
-			break;//跳出循环语句
-		}
-		cout << i << endl;
-
-	}
-
-	//continue语句：在循环语句中跳过本次循环，执行余下未执行的语句
-	//break跳出循环，不再循环了
-	/*for (int i = 0; i < 100; i++)
-	{
-		if (i%2 ==0)
-		{
-			continue;
-
-		}
-		cout << i << endl;
-	}*/
-	//arr[5]五个数，但是下标从0开始
-	/*int arr[5];
-	arr[0] = 10;
-	arr[1] = 11;
-	arr[2] = 12;
-	arr[3] = 13;
-	arr[4] = 14;*/
-
-	//
-	int arr2[5] = { 10,20,30,40,50 };
-	cout << arr2[0] << endl;
-	for (int i = 0; i < 5; i++)
-	{
-		cout << arr2[i] << endl;
-	}
-	int arr3[] = { 20,30,30,40 };
-
-
-	//数组名称的用途
-	//1 通过数组明统计整个数组占用内存大小
-	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
-	cout << "整个数组占用内存空间为：" << sizeof(arr) << endl;
-	cout << "每个元素所占的内存空间为：" << sizeof(arr[0]) << endl;
-	cout << "元素个数" << sizeof(arr) / sizeof(arr[0]) << endl;
-
-
-	//
-
-
-	//2通过数组名查看数组的首地址
-	cout << "数组首地址" << arr << endl;
-	cout << "数组首地址" << (int)arr << endl;
-	cout << "数组第一个元素首地址" << (int)&arr[0] << endl;
-	cout << "数组第二个元素首地址" << (int)&arr[1] << endl;
-
-	//五个小猪称体重取最重
-	int arr4[5] = { 200,300,280,400,300 };
-	int max = 0;
-	for (int i = 0; i < 5; i++)
-	{
-		//认定一个最大值
-		if (arr4[i] > max)
-		{
-			max = arr4[i];
-		}
-
-	}
-	cout << "最重小猪为" << max << endl;
-
-	//数组元素逆
-
-	
 }
 
 
