c0821716d145f5285bac914aa92d65dfb6975c56
arme17 <liuyishan17@163.com>
2023-11-23 10:16:00 +0800

    静态变量和静态成员函数

--- test1.cpp
+++ test1.cpp
@@ -5,44 +5,62 @@
 using namespace std;
 #include <string>
 #include<ctime>
-//深拷贝和浅拷贝
-class phone
-{
-public:
-	phone(string pname)
-	{
-		cout << "phone构造函数调用" << endl;
-		m_pname = pname;
-	}
-	string m_pname;
-	~phone()
-	{
-		cout << "1" << endl;
-	}
-};
 class person
 {
-public :
-	person(string name, string pname) :m_Name(name), m_phone(pname) {
-		cout << "person构造函数调用" << endl;
+	
+public:
+
+	//静态成员变量
+	// 所有对象共享同一份数据
+	//编译阶段就分配内存
+	//类内声明，类外初始化
+
+	static int m_a;
+	int b;
+
+
+	//静态成员函数
+	static void func()
+	{
+		//b= 200;静态成员函数不可以访问非静态成员变量，因为无法区分到底是哪一个对象的b;
+		m_a = 300;
+		cout << "static void func调用" << endl;
 	}
-	string m_Name;
-	phone m_phone;
-	~person() {
-		cout << "2" << endl;
-	}
+private:
+	static int m_b;
+
 };
-void test()
+ int person:: m_a = 0;
+void test01(){
+	person p;
+	cout << p.m_a << endl;
+	person p1;
+	p1.m_a = 200;
+	cout << p.m_a << endl;//200,所有对象共享同一份数据
+}
+void test02() {
+	//静态变量不属于某个对象上，所有对象共享同一份数据
+	//静态成员变量有两种访问方式
+	//1通过对象进行访问
+
+	//2通过类名进行访问 
+	cout << "m_a" << person::m_a << endl;
+	//cout << "m_b" << person::m_b << endl;私有静态成员变量也不可以访问
+}
+
+void test03()
 {
-	person p("张三", "iphone");
-	cout << p.m_Name <<endl;
-	//这里注意要先用p.m_phone再点mpname
-	cout << p.m_phone.m_pname<< endl;
+	//1通过对象访问
+	person p;
+	p.func();
+	//2通过类名访问
+	person::func();
 }
 int main()
 {
-	test();
-	
-	
+
+	test01();
+	test02();
+	test03();
 }
 
