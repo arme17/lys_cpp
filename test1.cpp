c07190c419e60b42461b3458fef3c9e8d8604160
arme17 <liuyishan17@163.com>
2023-11-23 16:35:14 +0800

    全局函数做友元

--- test1.cpp
+++ test1.cpp
@@ -5,44 +5,71 @@
 using namespace std;
 #include <string>
 #include<ctime>
-class person {
-public:
+
+//空指针调用成员函数
+class person
+{
+public :
+	void showclassname()
+	{
+		cout << "this is person class" << endl;
+	}
+	int m_age;
+	void showpersonage()
+	{
+		//健壮性
+		if (this==NULL)
+		{
+			return;
+		}
+		cout << "age =" << m_age<<endl;
+	}
+	//常函数，const修饰的是this指针，this指针的指向不可以修改，指向的值不可以改，除非用mutable修饰变量
+	void showperson() const 
+	{
+		this->m_b = 100;
+	}
+	mutable int m_b;
 	int m_a;
-	static int m_b;
-	void fun(){}
-	static void func(){}
-
-
-	//this 指针
-	person(int age)
-	{
-		this->age = age;
-		
-	}
-	person personAddAge(person& p)
-	{
-		this->age += p.age;
-
-		//*this指向的是p2这个对象本体
-		return *this;
-	}
-	int age;
 };
-//解决名称冲突
-//返回对象本身this
+class Building {
+	friend void goodGay(Building* building);
+public:
+	Building()
+	{
+		m_sittingRome = "客厅";
+		m_bedRoom = "卧室";
+	}
+public:
+	string m_sittingRome;
+private:
+	string m_bedRoom;
+};
+
 void test01()
 {
-	person p1(10);
-	cout << p1.age << endl;
-	person p2(10);
-//链式编程思想 40
-	p2.personAddAge(p1).personAddAge(p1).personAddAge(p1);
-	cout << p2.age << endl;
+	/*person* p = NULL;
+	p->showClassName();
+	 p->showPersonAge();*/
+	 const person p;//常对象
+	// p.showPerson();
+	 p.m_b = 100;
+	 //p.m_a = 200;
+
+	
+}
+//全局函数做友元
+void goodGay(Building *building)
+{
+	cout << "好基友全局函数在访问:" << building->m_sittingRome << endl;
+	cout << "好基友全局函数在访问:" << building->m_bedRoom << endl;
+
 }
 
 int main()
 {
-
+	Building b;
+	goodGay(&b);
 	test01();
 	
 }
