7e783696a932b8d231bc7d0975d3a51e2ad9a743
arme17 <liuyishan17@163.com>
2023-11-25 11:15:41 +0800

    虚析构和纯虚析构

--- test1.cpp
+++ test1.cpp
@@ -5,79 +5,56 @@
 using namespace std;
 #include <string>
 #include<ctime>
-//多态案例2，制作饮品
-//煮水，冲泡，倒入杯中，加入辅料
-class AbstractDrinking
+class Animal
 {
 public:
-	//煮水
-	virtual void Boil() = 0;
-	//冲泡
-	virtual void Brew() = 0;
-	//倒入杯中
-	virtual void PourInCup() = 0;
-	//加入辅料
-	virtual void PourSomeThing() = 0;
-
-	void makeDrink()
+	Animal()
 	{
-		Boil();
-		Brew();
-		PourInCup();
-		PourSomeThing();
+		cout << "animal构造函数调用" << endl;
 	}
+	//virtual ~Animal()//父类的析构作为虚函数，可以走子类的析构
+	//	//利用虚析构可以解决父类指针释放子类对象时不干净的问题
+	//{
+	//	cout << "animal析构函数调用" << endl;
+	//}
+	virtual ~Animal() = 0;//纯虚析构必须有具体实现，上面虚析构也有具体实现
+	//有纯虚析构在是抽象类，无法实例化对象
+	virtual void speak() = 0;
 };
-class Coffee :public AbstractDrinking
+Animal::~Animal()
 {
-public:
-	virtual  void Boil()
-	{
-		cout << "煮农夫水" << endl;
-	}
-	virtual void Brew()
-	{
-		cout << "冲泡咖啡" << endl;
-	}
-	virtual void PourInCup()
-	{
-		cout << "倒入杯中" << endl;
-	}
-	virtual void PourSomeThing()
-	{
-		cout << "加入糖和牛奶" << endl;
-	}
-};
-class Tea :public AbstractDrinking
-{
-public:
-	virtual  void Boil()
-	{
-		cout << "煮农夫水" << endl;
-	}
-	virtual void Brew()
-	{
-		cout << "冲泡茶水" << endl;
-	}
-	virtual void PourInCup()
-	{
-		cout << "倒入杯中" << endl;
-	}
-	virtual void PourSomeThing()
-	{
-		cout << "加入柠檬" << endl;
-	}
-};
-void doWork(AbstractDrinking* abs)//，如何调用里面的接口
-//AbstractDrinking* abs = new Coffee;父类的指针指向子类的对象，
-{
-	abs->makeDrink();
-	delete abs;//手动释放
+	cout << "animal纯虚析构函数调用" << endl;
 }
+class Cat :public Animal
+{
+public :
+
+	Cat(string name)
+	{
+		cout << "cat 构造函数调用" << endl;
+		m_name = new string(name);
+	}	
+	virtual void speak()
+	{
+		cout << *m_name<<" miao~" << endl;
+	}
+	~Cat()
+	{
+		if (m_name!=NULL)
+		{
+			cout << "cat析构" << endl;
+			m_name = NULL;
+		}
+	}
+	string* m_name;
+};
 void test01()
 {
-	doWork(new Coffee);//new 关键字在堆区开辟内存，
-	doWork(new Tea);
+	Animal* animal = new Cat("tom");
+	animal->speak();
+	delete animal;
 }
+
 int main()
 {
 	test01();
